815999cb124c752cba888678aadae2e47b182cfd
TedBarbier <ted.barbier@insa-cvl.fr>
2025-06-16 11:12:37 +0200

    Render Update add file for Render change in app.py for Render change fatch url to fetch on render backend

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import os
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from collections import Counter
@@ -40,3 +41,4 @@ def route_cesar():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=False, host="0.0.0.0", port=port)
+
