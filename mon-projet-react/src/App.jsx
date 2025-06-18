75b47a487022c43cbc3951a3788d979cfc0bcaca
TedBarbier <ted.barbier@insa-cvl.fr>
2025-06-18 14:42:43 +0200

    add react demo

---
+++ mon-projet-react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CryptoAnalyzer from './components/CryptoAnalyzer';
+
+function App() {
+  return (
+    <div className="App">
+      <CryptoAnalyzer />
+    </div>
+  );
+}
+
+export default App;
